81b760ad3eb0d9b776544a935f147419669fb68c
Adrian Kobusiński <57993450+AdrianKobusinski@users.noreply.github.com>
2020-10-27 15:02:00 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,32 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD - wiele do wielu</title>
-    <link rel="stylesheet" href="style1.css">
+    <title>CRUD wiele do wielu</title>
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <header>
-        <h1>Adrian Kobusiński</h1>
-    </header>
-    <main>
-    <?php
-    
-    require_one('lib/connect.php');
-    
-    
-    $sql = "SELECT * FROM lib_autor";
-    $result = $conn->query($sql);
-    while($row = $result->fetch_assoc()) {
-        echo ("id: " . $row["name"]."<br>");
-    }
+    <div class="grid">
+        <div class="header">
+            <h1>Adrian Kobusiński</h1>
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="">LINK1</a></li>
+                <li><a href="">LINK2</a></li>
+            </ul>
+        </div>
+        <div class="footer">
+            <form action="insert.php" method="POST">
+                <ul>
+                    <li>name:<input type="text" name="name"></li>
+                    <li>tytyl:<input type="text" name="tytul"></li>
+                    <li><input type="submit" value="INSERT"></li>
+                </ul>
+            </form>
+        </div>
+        <div class="main">
+        <?php
+            echo("START");
+            $servername="127.0.0.1";
+            $username="root";
+            $password="";
+            $dbname="szkola_zadnie";
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            $result = $conn->query();
+            
+            echo("<table>");
+            while($wiersz = $result->fetch_assoc()){
+                echo("tr");
+                echo("<td>".$wiersz['']."</td>");
+                echo("/tr");
+            }
+            echo("</table>");
+        ?> 
+        </div>
+    </div>
 
-    $conn->close();
-    echo("start");
-    ?>
-    </main>
-    <footer>
-    <a href="https://heroku.com">Link</a>
-    </footer>
+    
 </body>
-<script src="main.js"></script>
 </html>
